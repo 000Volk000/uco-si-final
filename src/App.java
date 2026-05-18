914f44832aa27c2f83ab26824327c84eb6b719c2
Darío Martínez Kostyuk <dariomartk@gmail.com>
2026-05-18 17:56:44 +0200

    feat: created initial canva

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Canva creation
+        JFrame jf = new JFrame("Pezqueñín");
+
+        jf.setSize(402, 874);
+        jf.setResizable(false);
+        jf.setLocationRelativeTo(null);
+
+        // TODO Background
+
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setVisible(true);
     }
 }
