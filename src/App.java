f79f19a938f76a96d17163ab85a537a4ca7e183a
Darío Martínez Kostyuk <dariomartk@gmail.com>
2026-05-18 18:16:52 +0200

    feat: imported escamas but not resized

--- src/App.java
+++ src/App.java
@@ -1,4 +1,31 @@
 import javax.swing.*;
+import java.awt.*;
+
+class BackgroundPanel extends JPanel {
+    private Image topLeft;
+    private Image lowLeft;
+    private Image topRight;
+
+    public BackgroundPanel() {
+        topLeft = new ImageIcon("src/assets/topLeft.png").getImage();
+        lowLeft = new ImageIcon("src/assets/bottomLeft.png").getImage();
+        topRight = new ImageIcon("src/assets/topRight.png").getImage();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int width = getWidth();
+        int height = getHeight();
+
+        if (topLeft != null)
+            g.drawImage(topLeft, 0, 0, this);
+        if (lowLeft != null)
+            g.drawImage(lowLeft, 0, height - lowLeft.getHeight(this), this);
+        if (topRight != null)
+            g.drawImage(topRight, width - topRight.getWidth(this), 0, this);
+    }
+}
 
 public class App {
     public static void main(String[] args) {
@@ -9,7 +36,11 @@ public class App {
         jf.setResizable(false);
         jf.setLocationRelativeTo(null);
 
-        // TODO Background
+        // Background
+        BackgroundPanel bgPanel = new BackgroundPanel();
+        bgPanel.setBackground(Color.decode("#B0C2DB"));
+
+        jf.setContentPane(bgPanel);
 
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.setVisible(true);
